83b2f1ecfdb7ae427e12d4565cbbdb5b4537d978
emmanuelpali <palinendir@gmail.com>
2024-03-28 10:31:41 -0600

    adding user details update route

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -8,6 +8,7 @@ const pino = require('pino');  // Import Pino logger
 dotenv.config();
 const logger = pino();  // Create a Pino logger instance
 
+const { body, validationResult } = require('express-validator');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res, next) => {
@@ -88,5 +89,61 @@ router.post('/login', async (req, res, next) => {
     }
 
 });
+
+// update user details
+router.put('/update', async (req, res, next) => {
+    //  Validate the request body
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        logger.error('Validation erros in updating user details', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+    // check if email is in the request body
+    try {
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not provided');
+            return res.status(400).json({ error: 'Email not provided' });
+        }
+
+        //connect to the database
+        const db = await connectToDatabase();
+        const collection = db.collection('users');
+
+        //find user credentials
+        const existingUser = await collection.findOne({ email });
+
+        if(!existingUser){
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        existingUser.firstName = req.body.firstname;
+        existingUser.lastName = req.body.lastname;
+        existingUser.updatedAt = new Date();
+
+        //update user details in database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+
+        // create jwt token with user._id as payload using secret key from .env file
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+        logger.info('User details updated successfully');
+        return res.status(200).json({ authtoken });
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).send("Internal Server Error");
+    }
+})
 
 module.exports = router;
